0db06ec7aec7e0c001dd84afb6ef44125f25d1eb
iFB24M <fakem1ner@yandex.ru>
2024-02-05 01:16:38 -0800

    add some minor updates remove "test enviroment" from release logo remove new year logo

--- src/components/Logo/Logo.component.tsx
+++ src/components/Logo/Logo.component.tsx
@@ -2,18 +2,19 @@ import type { ReactNode } from 'react'
 import styles from './Logo.module.scss'
 import Link from 'next/link'
 import Image from 'next/image'
+import { cookies } from 'next/headers'
 
 export const Logo = (): ReactNode => {
 	return (
 		<Link prefetch={false} href="/" className={styles.logo}>
-			<Image width={32} height={32} className={styles.icon} src="/happy-next-link.png" alt="fb24m Logo" />
+			<Image width={32} height={32} className={styles.icon} src="/logo.png" alt="fb24m Logo" />
 			<div className={styles.content}>
 				<span className={styles.title}>
 					NextLink
 				</span>
-				<span className={styles.subtitle}>
+				{process.env.IS_TEST_ENVIROMENT === '1' ? <span className={styles.subtitle}>
 					Test Enviroment, 0.3.0-beta.18
-				</span>
+				</span> : ''}
 			</div>
 		</Link>
 	)
