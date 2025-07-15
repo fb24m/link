262445f223b6cb0d0cd8afa846f7ff550ce00167
fb24m <hello@fb24m.ru>
2025-07-16 03:05:32 +0400

    add avatar change functionality

---
+++ src/shared/api/avatar.ts
@@ -0,0 +1,56 @@
+"use server";
+
+import { prisma } from "@/services/Prisma.service";
+import { v2 as cloudinary } from "cloudinary";
+import { users } from "./users";
+import { revalidatePath } from "next/cache";
+import { revalidateTag } from "next/cache";
+
+cloudinary.config({
+  api_key: process.env.CLOUDINARY_API_KEY,
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+export const setAvatar = async (
+  _: unknown,
+  formData: FormData,
+): Promise<string> => {
+  const avatar = formData.get("avatar") as File;
+
+  if (!avatar) {
+    throw new Error("Нет файла");
+  }
+
+  const arrayBuffer = await avatar.arrayBuffer();
+
+  const base64 = Buffer.from(arrayBuffer).toString("base64");
+
+  const blob = new Blob([avatar], { type: avatar.type });
+
+  const dataUrl = `data:${avatar.type};base64,${base64}`;
+
+  const file = await cloudinary.uploader.upload(dataUrl, {});
+
+  const me = await users.getMe();
+
+  await prisma.user.update({
+    where: {
+      id: me.id,
+    },
+    data: {
+      avatar: file.secure_url ?? "",
+    },
+  });
+
+  revalidatePath("/profile/*");
+  revalidatePath("/profile");
+  revalidatePath(`/user/${me.username}`);
+  revalidatePath(`/user/${me.username}/*`);
+  revalidateTag(`user/${me.id}`);
+  revalidateTag(`user/${me.username}`);
+
+  return "finished";
+
+  // curl https://api.cloudinary.com/v1_1/<CLOUD_NAME>/image/upload -X POST --data 'file=<FILE>&timestamp=<TIMESTAMP>&api_key=<API_KEY>&signature=<SIGNATURE>'
+};
